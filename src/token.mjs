a5326bde44676c85ec3ead7fa3ffc256c6537b3a
Povilas Simanaitis <mr.simanaitis@gmail.com>
2020-02-04 00:40:50 +0200

    Jest cleanup. Makes mocha runnable on nodemon. Adds getAllTags function.

--- src/token.mjs
+++ src/token.mjs
@@ -1,10 +1,32 @@
 export const generateTokens = (tokens) => Object.keys(tokens).reduce((acc, key) => {
   acc.push(`<${key} style="`);
-  acc.push(`</${key}>`);
   tokens[key].forEach((item) => {
     const styleNames = Object.keys(item);
     const styleValues = styleNames.map((styleName) => item[styleName].map((value) => `${styleName}:${value};`)).flat();
     acc = [...acc, ...styleValues];
   });
+  acc.push(`</${key}>`);
   return acc;
-}, []);
+}, [`">`]);
+
+export const getAllTags = (tokenList)=>{
+  const openingTag = tokenList.find((token) => token.match(/<*. style=/));
+  const clostingTag = tokenList.find((token) => token.match(/<\//));
+  const closingOpeningPart = `">`;
+  const uniqueKeys = tokenList
+      .filter((item) => item !== openingTag && item !== clostingTag && item !== closingOpeningPart)
+      .reduce((acc, item)=>({...acc, [item.split(':')[0]]: [...(acc[item.split(':')[0]] ? acc[item.split(':')[0]] : []), item]}), {});
+
+  return expandWithAll(Object.values(uniqueKeys), (data)=>data)
+      .map((stylePart)=> ((content)=>`${openingTag}${stylePart}${closingOpeningPart}${content}${clostingTag}`));
+};
+
+const expandWithAll = (collections, content)=>{
+  if (collections.length === 1) {
+    return collections[0].map(content);
+  } else {
+    return collections[0].map((item)=>expandWithAll(collections.slice(1, collections.length), (data)=>
+      `${item} ${data}`
+    )).flat();
+  }
+};
--- src/token.test.mjs
+++ src/token.test.mjs
@@ -1,14 +1,16 @@
 import * as assert from 'assert';
-import {generateTokens} from './token.mjs';
+import {generateTokens, getAllTags} from './token.mjs';
+import 'mjs-mocha';
 
 const sameElements = (first, second) => first.every((item) => second.includes(item)) && second.every((item) => first.includes(item));
 
 describe('token', () => {
-  it('should generate tokens from given tree', () => {
+  it('generateTokens generates list of all posible tokens', () => {
     const tokensConfiguration = {
       div: [{'display': ['flex'], 'flex-direction': ['row', 'collumn']}],
     };
     const expectedResults = [
+      '">',
       '<div style="',
       'display:flex;',
       'flex-direction:row;',
@@ -17,6 +19,24 @@ describe('token', () => {
     ];
 
     const result = generateTokens(tokensConfiguration);
+
+    assert.equal(sameElements(result, expectedResults), true);
+  });
+
+  it('generateTokens generates list of all posible html tags', () => {
+    const tokensConfiguration = {
+      div: [{'display': ['flex', 'inline-flex'], 'flex-direction': ['row', 'collumn', 'reverse-row']}],
+    };
+    const expectedResults = [
+      '<div style="display:flex; flex-direction:row;"></div>',
+      '<div style="display:flex; flex-direction:collumn;"></div>',
+      '<div style="display:flex; flex-direction:reverse-row;"></div>',
+      '<div style="display:inline-flex; flex-direction:row;"></div>',
+      '<div style="display:inline-flex; flex-direction:collumn;"></div>',
+      '<div style="display:inline-flex; flex-direction:reverse-row;"></div>',
+    ];
+
+    const result = getAllTags(generateTokens(tokensConfiguration)).map((item)=>item(''));
 
     assert.equal(sameElements(result, expectedResults), true);
   });
